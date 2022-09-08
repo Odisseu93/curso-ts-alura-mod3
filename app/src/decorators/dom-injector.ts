3540307daa040e73b3fd5bb7b4538b5c861496ec
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-09-07 21:52:49 -0300

    criando e implementando o dom-Injector

---
+++ app/src/decorators/dom-injector.ts
@@ -0,0 +1,20 @@
+export function domInjector(seletor: string) {
+  return function(target: any, propertyKey: string) {
+
+      console.info('modificando protype', target.constructor.name,
+      '\ne adicionando getter para a propriedade', propertyKey);
+      const getter = function() {
+          const elemento = document.querySelector(seletor);
+          console.info('buscando elemento do DOM como seletor\n',seletor, 'para injetar em', propertyKey);
+          return elemento;
+      }
+
+
+      Object.defineProperty(
+        target,
+        propertyKey, 
+        { get: getter }
+      );
+
+  }
+}
